a92e0ee907a86975095cc421a55a50e67f03f438
Kite <ixkaito@gmail.com>
2016-05-27 19:29:12 +0900

    Improve

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ $(function() {
 
   $('.js-data').on('click', function() {
     foo = $('.js-foo').data('foo');
-    console.log(foo);
     $('.js-foo').data('foo', foo == 'foo' ? 'bar' : 'foo');
+    console.log($('.js-foo').data('foo'));
   });
 
   $('.js-attr').on('click', function() {
     foo = $('.js-foo').attr('data-foo');
-    console.log(foo);
     $('.js-foo').attr('data-foo', foo == 'foo' ? 'bar' : 'foo');
+    console.log($('.js-foo').attr('data-foo'));
   });
 
 });
